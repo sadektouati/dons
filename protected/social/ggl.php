5889d8b974b90d6a04f37ab572867224a7a1fc18
sadektouati <bilal2dellys@hotmail.com>
2023-09-23 19:43:57 -0400

    debuggin...

--- protected/social/ggl.php
+++ protected/social/ggl.php
@@ -24,4 +24,13 @@ $socialProfilePage = $google_account_info->email;
 $socialUserEmail = $google_account_info->email;
 $socialUserName = $google_account_info->name;
 
-include_once('protected/social/signin.php');
+$signinUserQuery = pg_query_params($conn, "select * from app.signin_doner($1)", [$socialId]);
+$signinUserResult = pg_fetch_assoc($signinUserQuery);
+
+$_SESSION['social_id'] = $socialId;
+
+?>
+<main>
+    <header>You're signed in successfully :)</header>
+    <meta http-equiv="refresh" content="0; url=/profile">
+</main>
