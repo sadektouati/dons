e7d2d85e151cdd32cd527775b9ca971eb137b3df
sadektouati <bilal2dellys@hotmail.com>
2023-09-23 16:30:50 -0400

    debugin...

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php session_start();
-	ini_set('display_errors', 1);
-	ini_set('display_startup_errors', 1);
-	error_reporting(E_ALL);
+
+	// ini_set('display_errors', 1);
+	// ini_set('display_startup_errors', 1);
+	// error_reporting(E_ALL);
+
 $_SESSION['social_id'] = $_SESSION['social_id'] ?? null;
 
 require('vendor/autoload.php');
